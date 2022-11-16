b48e37d7c11d8a5075524403dda7f95466e56c6b
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2022-11-16 17:56:55 -0500

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 g++ -O4 -g svdDynamic.c RayTracer.c utils.c -lm -o RayTracer
 
-./RayTracer ./RayTracer.exe 1024 3 1 hierarchical.ppm 1
+./RayTracer 1024 3 1 hierarchical.ppm 1
 
-./RayTracer ./RayTracer.exe 1024 3 1 basic.ppm 2
+./RayTracer 1024 3 1 basic.ppm 2
 
-./RayTracer ./RayTracer.exe 1024 3 1 final.ppm 3
+./RayTracer 1024 3 1 final.ppm 3
